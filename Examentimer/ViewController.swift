2c6d11191d9c9dbf0b281462545b653b71fa8cec
Benjamin Schnabel <Benjamin-777@gmx.de>
2019-06-11 12:58:57 +0200

    Initial Commit

---
+++ Examentimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examentimer
+//
+//  Created by Benjamin Schnabel on 11.06.19.
+//  Copyright © 2019 Acheros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ExamentimerTests/ExamentimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExamentimerTests.swift
+//  ExamentimerTests
+//
+//  Created by Benjamin Schnabel on 11.06.19.
+//  Copyright © 2019 Acheros. All rights reserved.
+//
+
+import XCTest
+@testable import Examentimer
+
+class ExamentimerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
